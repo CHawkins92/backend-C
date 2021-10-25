8a5317c14240098e7b6ce3f86e165c066b64c5b5
CHawkins92 <conor.hawkins@gmx.com>
2021-10-25 13:10:01 +0000

    Initialize project.

---
+++ src/test/java/com/example/backendgroup/BackendgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendgroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
