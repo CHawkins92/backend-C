0e078f5f4c12ec23cb1cc1d38821d6d82486ff71
CHawkins92 <conor.hawkins@gmx.com>
2021-10-25 13:10:01 +0000

    Initialize project.

---
+++ src/main/java/com/example/backendgroup/BackendgroupApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendgroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendgroupApplication.class, args);
+    }
+
+}
